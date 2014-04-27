3a1c0a7c9e4c6bf0742251eec34b46bfffee5081
Alessandro Minoccheri <alessandro.minoccheri@gmail.com>
2014-04-27 16:56:42 +0200

    make twig works

---
+++ app/Plugin/TwigView/tmp/views/52/91/2dc0a92ff93eee535488f4a1f759f8428e744c37ff99f8deb8adc9a2c968.php
@@ -0,0 +1,31 @@
+<?php
+
+/* Examples/index.twig.tpl */
+class __TwigTemplate_52912dc0a92ff93eee535488f4a1f759f8428e744c37ff99f8deb8adc9a2c968 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<p>Test</p>";
+    }
+
+    public function getTemplateName()
+    {
+        return "Examples/index.twig.tpl";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
